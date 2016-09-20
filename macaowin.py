5ba126ae5d44a0971f3c93cf96d314181ae799b2
daimon@air <daijian1@qq.com>
2016-09-20 11:54:37 +0800

    enhance

--- macaowin.py
+++ macaowin.py
@@ -119,6 +119,8 @@ def go(wallet, bet):
 
         if me.money < 0: break
     click.echo('没钱了。。。')
+    power = game.max_money / my_wallet
+    click.echo('但你曾经是 %s 倍神!' % int(power))
 
 
 if __name__ == '__main__':
